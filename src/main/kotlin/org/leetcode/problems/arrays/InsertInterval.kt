04712b267791ba4766b5a55a7f946793f89664cb
David Rojas <davidrojas@Mac.attlocal.net>
2025-04-27 17:23:27 -0400

    add more arrays questions

--- src/main/kotlin/org/leetcode/problems/arrays/InsertInterval.kt
+++ src/main/kotlin/org/leetcode/problems/arrays/InsertInterval.kt
@@ -8,6 +8,22 @@ package org.leetcode.problems.arrays
  */
 class InsertInterval {
     fun main(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
-        return arrayOf()
+        val result = mutableListOf<IntArray>()
+        var mutableNewInterval = newInterval
+
+        for (interval in intervals) {
+            if (interval[1] < mutableNewInterval[0]) {
+                result.add(interval)
+            } else if (interval[0] > mutableNewInterval[1]) {
+                result.add(newInterval)
+                mutableNewInterval = interval
+            } else {
+                mutableNewInterval[0] = Math.min(mutableNewInterval[0], interval[0])
+                mutableNewInterval[1] = Math.max(mutableNewInterval[1], interval[1])
+            }
+        }
+
+        result.add(mutableNewInterval)
+        return result.toTypedArray()
     }
 }
